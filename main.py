dbf6f6e21dbce83d9ce714b1231d08b6dd06bdbf
Nishchal Ravish <ghanteravish@gmail.com>
2025-03-11 23:12:49 -0500

    Initial Push

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils import read_video, save_video
+
+def main():
+    # Read Video
+    video_frames = read_video('input_videos/test.mp4')
+    
+    # Save video
+    save_video(video_frames,'output_videos/output_video.avi')
+    
+if __name__ == "__main__":
+    main()
