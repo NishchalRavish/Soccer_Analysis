656a6d5e344735e7d13165bdff08b19ffc7745b6
Nishchal Ravish <ghanteravish@gmail.com>
2025-03-20 23:25:13 -0500

    Player Rectangle Box

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -4,6 +4,7 @@ import pickle
 import os
 import sys
 import cv2
+import numpy as np
 
 sys.path.append('../') # Goes 1 dir up and if we need to go 2 dir up, we need to do ('../..')
 from utils import get_center_of_bbox, get_bbox_width
@@ -83,7 +84,7 @@ class Tracker:
         return tracks 
     
     
-    def draw_ellipse(self, frame, bbox, color, track_id):
+    def draw_ellipse(self, frame, bbox, color, track_id=None):
         y2 = int(bbox[3])
         
         x_center, _ = get_center_of_bbox(bbox)
@@ -94,12 +95,56 @@ class Tracker:
             center = (x_center,y2),
             axes = (int(width), int(0.35*width)),
             angle = 0.0,
-            startAngle = 45,
+            startAngle = -45,
             endAngle = 235,
             color = color,
             lineType = cv2.LINE_4
         ) 
 
+        # Rectangle box for player number
+        rectangle_width = 40
+        rectangle_height = 20
+        x1_rect = x_center - rectangle_width//2
+        x2_rect = x_center + rectangle_width//2
+        y1_rect = (y2 - rectangle_height//2) + 15
+        y2_rect = (y2 + rectangle_height//2) + 15
+        
+        if track_id is not None:
+            cv2.rectangle(frame,
+                          (int(x1_rect), int(y1_rect)),
+                          (int(x2_rect), int(y2_rect)),
+                          color,
+                          cv2.FILLED)
+                          
+            x1_text = x1_rect + 12
+            if track_id > 99:
+                x1_text -=10
+            
+            cv2.putText(
+                frame,
+                f"{track_id}",
+                (int(x1_text), int(y1_rect+15)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6, #font
+                (0,0,0), # color
+                2 #Thickness
+            )
+            
+        return frame
+    
+    def draw_traingle(self, frame, bbox, color):
+        y = int(bbox[1])
+        x, _ = get_center_of_bbox(bbox)
+        
+        triangle_points = np.array([
+            [x,y],
+            [x-10, y-20],
+            [x+10, y-20],
+        ])
+        
+        cv2.drawContours(frame, [triangle_points], 0, color, cv2.FILLED)
+        cv2.drawContours(frame, [triangle_points], 0, (0,0,0), 2)
+        
         return frame
         
         
@@ -108,14 +153,24 @@ class Tracker:
         for frame_num, frame in enumerate(video_frames):
             frame = frame.copy()
             
-            player_dict = tracks['players'][frame_num]
-            ball_dict = tracks['ball'][frame_num]
-            referees_dict = tracks['referees'][frame_num]
+            player_dict = tracks["players"][frame_num]
+            ball_dict = tracks["ball"][frame_num]
+            referees_dict = tracks["referees"][frame_num]
             
             # Draw Players
             
             for track_id, player in player_dict.items():
                 frame = self.draw_ellipse(frame, player['bbox'], (0,0,255), track_id)
+            
+            # Draw Referee
+            
+            for _, referee in referees_dict.items():
+                frame = self.draw_ellipse(frame, referee['bbox'], (0,255,255), track_id)
+            
+            # Draw ball
+            
+            for track_id, ball in ball_dict.items():
+                frame = self.draw_traingle(frame, ball['bbox'], (0,255,0))
                 
             output_video_frames.append(frame)
     
