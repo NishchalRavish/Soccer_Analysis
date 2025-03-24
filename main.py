b5244b1f77ed8acb3da20500777bcbda3273313b
Nishchal Ravish <ghanteravish@gmail.com>
2025-03-24 01:22:12 -0500

    View Transformer Added to Main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
+from view_transformer import ViewTransformer
 
 def main():
     # Read Video
@@ -26,6 +27,10 @@ def main():
                                                                               stub_path='stubs/camera_movement_stub.pkl')
     
     camera_movement_estimator.add_adjust_positions_to_tracks(tracks, camera_movement_per_frame)
+    
+    # View Transformer 
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(tracks)
     
     # Interpolate Ball Positions
     tracks['ball'] = tracker.interpolate_ball_positions(tracks['ball'])
